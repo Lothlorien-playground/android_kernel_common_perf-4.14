e3678b125b47b9164c2d7ff9feaa0a516c93560b
Chris Lew <clew@codeaurora.org>
2019-06-03 11:47:57 -0700

    soc: qcom: smp2p_sleepstate: Add suspend delay
    Add delay in smp2p_sleepstate suspend path to prevent wakeup loop with
    non wakeup streaming from sensors. This delay should be the maximum
    time for sensors samples in flight to be drained.
    
    Change-Id: I79f944caa2ccdd65dc1649aef8d6359f44612479
    Signed-off-by: Chris Lew <clew@codeaurora.org>
    Signed-off-by: Ananth Raghavan Subramanian <sananth@codeaurora.org>
    Signed-off-by: Kelly Rossmoyer <krossmo@google.com>
    Bug: 123377615
    Bug: 131260677

--- drivers/soc/qcom/smp2p_sleepstate.c
+++ drivers/soc/qcom/smp2p_sleepstate.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -18,6 +18,7 @@
 #include <linux/of_irq.h>
 #include <linux/of.h>
 #include <linux/pm_wakeup.h>
+#include <linux/delay.h>
 
 #define PROC_AWAKE_ID 12 /* 12th bit */
 #define AWAKE_BIT BIT(PROC_AWAKE_ID)
