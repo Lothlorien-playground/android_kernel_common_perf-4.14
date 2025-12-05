cf434b4404281d41e00be009e9ec0b5770b1fc3a
Sultan Alsawaf <sultan@kerneltoast.com>
2025-12-05 17:05:31 +0800

    proc: cmdline: Patch SafetyNet flags
    Userspace parses androidboot.* flags from /proc/cmdline and sets the
    ro.boot.* props accordingly, which in turn trips SafetyNet when the
    reported values are incorrect.
    
    Patch the cmdline flags checked by SafetyNet to prevent it from failing
    a device. These flags were found by extracting the latest snet.jar and
    searching for 'ro.boot.' strings.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,6 +3,9 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <asm/setup.h>
+
+static char new_command_line[COMMAND_LINE_SIZE];
 
 #if defined (CONFIG_INITRAMFS_IGNORE_SKIP_FLAG) \
 	|| defined(CONFIG_CMDLINE_HWC_IS_SKU) \
@@ -76,7 +79,7 @@ static int cmdline_proc_show(struct seq_file *m, void *v)
 #ifdef ALTER_CMDLINE
 	seq_printf(m, "%s\n", proc_command_line);
 #else
-	seq_printf(m, "%s\n", saved_command_line);
+	seq_printf(m, "%s\n", new_command_line);
 #endif
 	return 0;
 }
@@ -93,11 +96,41 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
+static void patch_flag(char *cmd, const char *flag, const char *val)
+{
+	size_t flag_len, val_len;
+	char *start, *end;
+
+	start = strstr(cmd, flag);
+	if (!start)
+		return;
+
+	flag_len = strlen(flag);
+	val_len = strlen(val);
+	end = start + flag_len + strcspn(start + flag_len, " ");
+	memmove(start + flag_len + val_len, end, strlen(end) + 1);
+	memcpy(start + flag_len, val, val_len);
+}
+
+static void patch_safetynet_flags(char *cmd)
+{
+	patch_flag(cmd, "androidboot.flash.locked=", "1");
+	patch_flag(cmd, "androidboot.verifiedbootstate=", "green");
+	patch_flag(cmd, "androidboot.veritymode=", "enforcing");
+}
+
 static int __init proc_cmdline_init(void)
 {
 #ifdef ALTER_CMDLINE
 	proc_command_line_init();
 #endif
+	strcpy(new_command_line, saved_command_line);
+
+	/*
+	 * Patch various flags from command line seen by userspace in order to
+	 * pass SafetyNet checks.
+	 */
+	patch_safetynet_flags(new_command_line);
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
